bd852270d567239f66a4823452689d75bb12d4bc
Siddharth <siddharth261202@gmail.com>
2024-02-01 19:47:47 -0500

    added custom font

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    VANTA.WAVES({
+        el: "#your-element-selector",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x512aaa
+    })
+})
