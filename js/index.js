effbfad0a2973ee125a6e53cae423e4c78053fc0
Siddharth <siddharth261202@gmail.com>
2024-02-01 20:22:01 -0500

    added vanta.js background

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    VANTA.WAVES({
-        el: "#your-element-selector",
+    VANTA.NET({
+        el: "#main",
         mouseControls: true,
         touchControls: true,
         gyroControls: false,
         minHeight: 200.00,
         minWidth: 200.00,
         scale: 1.00,
-        scaleMobile: 1.00,
-        color: 0x512aaa
+        scaleMobile: 1.00
     })
 })
