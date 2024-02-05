044d30a553e155c63e02774b357568082a11df46
sgarg26 <siddharth261202@gmail.com>
2024-02-04 19:25:55 -0500

    better formatting

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-gsap.registerPlugin(TextPlugin)
+gsap.registerPlugin(TextPlugin);
 document.addEventListener("DOMContentLoaded", () => {
     VANTA.NET({
         el: "#vanta-bg",
         mouseControls: true,
         touchControls: true,
         gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00
-    })
-})
+        minHeight: 200.0,
+        minWidth: 200.0,
+        scale: 1.0,
+        scaleMobile: 1.0,
+    });
+});
 
-gsap.to(".display-5", { duration: 2, text: "Hi, I'm Siddharth Garg", delay: 1 })
+gsap.to(".display-5", { duration: 2, text: "Hi, I'm Siddharth Garg" });
+
