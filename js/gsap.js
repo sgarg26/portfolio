9b3578b8a3c7fe3c91eaf5fe4ea9648c70e506e2
sgarg26 <siddharth261202@gmail.com>
2024-02-06 10:49:18 -0500

    modal trials

--- js/gsap.js
+++ js/gsap.js
@@ -1,6 +1,16 @@
-let tl = gsap.timeline()
+/* let tl = gsap.timeline();
 
-tl.from(".box", {duration: 2.5, opacity: 0, scale: 0.1})
-tl.from(".circle", {duration: 1, opacity: 0, y: "random(-100,100)", onComplete: () => {
-    console.log("hello there, animation complete")
-    }})
+tl.from(".box", { duration: 2.5, opacity: 0, scale: 0.1 });
+tl.from(".circle", {
+    duration: 1,
+    opacity: 0,
+    y: "random(-100,100)",
+    onComplete: () => {
+        console.log("hello there, animation complete");
+    },
+});
+*/
+document.getElementById("trialBtn").addEventListener("click", () => {
+    console.log("clicked");
+});
+
