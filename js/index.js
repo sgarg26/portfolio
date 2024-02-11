04b8c291f57c1f123c275a38d1b7791a73c37763
Siddharth <siddharth261202@gmail.com>
2024-02-11 17:19:25 -0500

    making container-lg fixes spawning in columns

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 gsap.registerPlugin(TextPlugin);
 
-const DocListener = document.addEventListener;
-const ById = document.getElementById;
-let projectsCarouselShowing = false;
-
-DocListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     VANTA.NET({
         el: "#vanta-bg",
         mouseControls: false,
@@ -26,19 +22,3 @@ let tl = gsap
         duration: 2,
         stagger: 0.25,
     });
-
-const projects =
-    "-> I have worked extensively with creating script based and graphical mods for games like" +
-    "the Witcher 3, and Starfield.\n\n-> I like learning about OS operations and have used Windows Libraries to build a Volume Wheel\n" +
-    "\n-> I'm passionate about tools like Neovim, and have created an extensive configuration using Lua\n" +
-    "   Check out more of my projects on Nexus Mods, and GitHub!";
-
-const aboutMe =
-    "Welcome to my portfolio! I'm a CS student at George Mason University. My interests " +
-    "lie in writing code across diverse domains, including robotics and contributing to open source projects.";
-
-// document.getElementById("about-me-link").addEventListener("mouseup", () => {
-//     document.getElementsByClassName("modal-header")[0].innerText = "About Me";
-//     document.getElementsByClassName("modal-body")[0].innerText = aboutMe;
-//     document.getElementsByClassName("fs-4")[0].style.visibility = "hidden";
-// });
